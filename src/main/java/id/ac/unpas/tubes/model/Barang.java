d0b87f659fd798d7635e8ffba7e7ee694e076cdf
aufaramdhn <ramadhanaufa3@gmail.com>
2026-01-04 16:50:18 +0700

    menambahkan model Barang

--- src/main/java/id/ac/unpas/tubes/model/Barang.java
+++ src/main/java/id/ac/unpas/tubes/model/Barang.java
@@ -1,5 +1,20 @@
 package id.ac.unpas.tubes.model;
 
 public class Barang {
-  
+    private String id;
+    private String nama;
+    private String kategori;
+    private int stok;
+    private String lokasiRak;
+
+    public Barang(String id, String nama, String kategori, int stok, String lokasiRak) {
+        this.id = id;
+        this.nama = nama;
+        this.kategori = kategori;
+        this.stok = stok;
+        this.lokasiRak = lokasiRak;
+    }
+
+    public String getId() { return id; }
+    public String getNama() { return nama; }
 }
